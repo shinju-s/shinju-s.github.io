fdeabf4cde28753bf380c837b669d6f22aa1f509
shinju-s <306531115+shinju-s@users.noreply.github.com>
2026-08-15 15:43:40 +0900

    サイト情報を更新

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,6 +1,7 @@
 export const site = {
   title: "清水慎樹 Official Website",
-  description: "...",
+  description:
+    "清水慎樹の公式サイト。音楽作品、歌詞、映像、noteでの制作記録を紹介します。",
   url: "https://shinju-s.github.io",
   author: "清水慎樹",
 
@@ -9,5 +10,5 @@ export const site = {
     arms: "https://note.com/arms_s",
   },
 
-  youtube: "...",
+  youtube: "https://www.youtube.com/@arms_shinju",
 };
